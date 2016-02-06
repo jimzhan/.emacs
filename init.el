f07b79f0c50c5cfc4ff7438390bec91c843f94eb
jimzhan <jim.zhan@me.com>
2016-02-06 11:59:10 +0800

    * merge init-screen into init-editor * add ctrl-h supports (respect traditional *nix)

--- init.el
+++ init.el
@@ -32,7 +32,6 @@
 ;; Calls (package-initialize)
 (require 'init-elpa)      ;; Machinery for installing required packages
 (require 'init-exec-path) ;; Set up $PATH
-(require 'init-screen)
 
 
 ;;----------------------------------------------------------------------------
--- lisp/init-screen.el
+++
@@ -1,9 +0,0 @@
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
-
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-
-(provide 'init-screen)
